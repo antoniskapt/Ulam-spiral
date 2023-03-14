8b641f4fe4e1dc564687a50305c3d30157e467f7
Antonis Kapetanios <127683866+antoniskapt@users.noreply.github.com>
2023-03-14 19:47:28 +0200

    Add files via upload

---
+++ symbolic/prime.c
@@ -0,0 +1,28 @@
+#include "prime.h"
+#include <stdio.h>
+#include <math.h>
+
+bool isPrime(int** arr, int r, int c){
+  
+    int tmp=arr[r][c];
+    if(tmp==1) return false;
+    for(int k=2;k<=(int) sqrt(tmp);k++){
+        if(tmp%k==0)
+            return false;
+        else continue;
+    }
+    return true;
+}
+
+void print(int** arr, int dim){
+    printf("\n\033[38;5;0m");
+    for(int i=0;i<dim;i++){
+        for(int j=0;j<dim;j++)
+        if(isPrime(arr, i, j))
+            printf("\033[38;5;211m*\033[38;5;0m "); // make foreground pink, print * and restore foreground to black
+        else
+            printf("  ");
+        printf("\n");
+    }
+    printf("\n");
+}
---
+++ symbolic/prime.h
@@ -0,0 +1,11 @@
+#ifndef PRIME_H_INCLUDED
+#define PRIME_H_INCLUDED
+
+#include <stdbool.h>
+
+
+bool isPrime(int** arr, int r, int c);
+
+void print(int** arr, int dim);
+
+#endif
